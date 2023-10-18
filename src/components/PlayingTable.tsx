2df5fab2ca875921d254261c7ef3f765ede8f438
Jakub Korsak <jakub@korsak.xyz>
2023-10-18 13:58:18 +0200

    feat: Add the frame to active player

--- src/components/PlayingTable.tsx
+++ src/components/PlayingTable.tsx
@@ -1,11 +1,14 @@
+import { Spinner } from "react-bootstrap";
 import Player from "../interfaces/Player";
 
 type Props = {
   players: Player[];
   stakedChips: number;
+  isLoading: boolean;
+  currentPlayer: string;
 };
 
-const PlayingTable = ({ players, stakedChips }: Props) => {
+const PlayingTable = ({ players, stakedChips, isLoading, currentPlayer }: Props) => {
   const phi = (2 * Math.PI) / players.length;
 
   const coords = (i: number, radius: number) => {
@@ -15,23 +18,31 @@ const PlayingTable = ({ players, stakedChips }: Props) => {
     };
   };
 
+  const playerFrameCls = (player: Player) => {
+    if (player.id === currentPlayer)
+      return "player-frame frame-active";
+    return "player-frame";
+  }
+
   return (
     <div className="circular-table-wrapper">
-      {" "}
       <div className="circular-table" />
-      {players.map((player, index) => {
-        return (
-          <div key={player.id}>
-            <div className="player-frame" style={coords(index, 40)}>
-              <p className="player-frame-name">{player.name}</p>
-              <p className="player-frame-chips">${player.chips}</p>
+      {isLoading && <Spinner />}
+      {!isLoading &&
+        players.length > 0 &&
+        players.map((player, index) => {
+          return (
+            <div key={player.id}>
+              <div className={playerFrameCls(player)} style={coords(index, 40)}>
+                <p className="player-frame-name">{player.name}</p>
+                <p className="player-frame-chips">${player.chips}</p>
+              </div>
+              <p className="player-chips" style={coords(index, 20)}>
+                ${player.stakedChips}
+              </p>
             </div>
-            <p className="player-chips" style={coords(index, 20)}>
-              ${player.stakedChips}
-            </p>
-          </div>
-        );
-      })}
+          );
+        })}
       <p className="staked-chips">Pot: ${stakedChips}</p>
     </div>
   );

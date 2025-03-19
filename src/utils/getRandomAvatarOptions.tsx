884e0750e8517aeac7c2511bc724751f830d9843
Jakub Korsak <jakub@korsak.xyz>
2025-03-19 11:05:43 +0100

    feat: restrict available avatar props

--- src/utils/getRandomAvatarOptions.tsx
+++ src/utils/getRandomAvatarOptions.tsx
@@ -1,4 +1,4 @@
-import { avataaars } from '@dicebear/collection';
+import { avataaars } from "@dicebear/collection";
 
 const randomizeValue = (array: string[]) => {
   const randomIndex = Math.floor(Math.random() * array.length);
@@ -7,59 +7,83 @@ const randomizeValue = (array: string[]) => {
 
 const properties = avataaars.schema.properties;
 
+const REMOVED_PROPS = ["vomit", "eyepatch", "cry", "xDizzy"];
+
 const getRandomAvatarOptions = () => {
   if (!properties) {
     return [];
   }
   let enumProps: any = {};
-  
+
   for (const key in properties) {
     const property = properties[key];
 
-    if (typeof property === 'object' && property !== null && 'items' in property) {
+    if (
+      typeof property === "object" &&
+      property !== null &&
+      "items" in property
+    ) {
       const items = property.items;
-      
-      if (typeof items === 'object' && items !== null && 'enum' in items) {
+
+      if (typeof items === "object" && items !== null && "enum" in items) {
         const enumArray = items.enum;
-        
-        if (Array.isArray(enumArray) && enumArray.every(item => typeof item === 'string')) {
-          enumProps[key] = randomizeValue(enumArray as string[]);
+
+        if (
+          Array.isArray(enumArray) &&
+          enumArray.every((item) => typeof item === "string")
+        ) {
+          const restrictedProps = (enumArray as string[]).filter(
+            (o) => !REMOVED_PROPS.includes(o),
+          );
+          enumProps[key] = randomizeValue(restrictedProps);
         }
       }
     }
 
-    if (key.includes("Color") && typeof property === 'object' && property !== null) {
-      if ('default' in property && Array.isArray(property.default) && property.default.every(item => typeof item === 'string')) {
+    if (
+      key.includes("Color") &&
+      typeof property === "object" &&
+      property !== null
+    ) {
+      if (
+        "default" in property &&
+        Array.isArray(property.default) &&
+        property.default.every((item) => typeof item === "string")
+      ) {
         enumProps[key] = randomizeValue(property.default as string[]);
       }
     }
 
     if (key.includes("Probability")) {
-      enumProps[key] = Math.random() >= 0.5 ? 100 : 0;
+      if (key.includes("facialHairProbability")) {
+        enumProps[key] = 0;
+      } else {
+        enumProps[key] = Math.random() >= 0.5 ? 100 : 0;
+      }
     }
   }
 
   return enumProps;
 };
 
-export default getRandomAvatarOptions
+export default getRandomAvatarOptions;
 
 export interface AvatarOptions {
-    accessories: string;
-    accessoriesColor: string;
-    accessoriesProbability: number;
-    clothesColor: string;
-    clothing: string;
-    clothingGraphic: string;
-    eyebrows: string;
-    eyes: string;
-    facialHair: string;
-    facialHairColor: string;
-    facialHairProbability: number;
-    hairColor: string;
-    hatColor: string;
-    mouth: string;
-    skinColor: string;
-    top: string;
-    topProbability: number;
-};
+  accessories: string;
+  accessoriesColor: string;
+  accessoriesProbability: number;
+  clothesColor: string;
+  clothing: string;
+  clothingGraphic: string;
+  eyebrows: string;
+  eyes: string;
+  facialHair: string;
+  facialHairColor: string;
+  facialHairProbability: number;
+  hairColor: string;
+  hatColor: string;
+  mouth: string;
+  skinColor: string;
+  top: string;
+  topProbability: number;
+}
